94c86ec9348da5b4bdc9e389465a44f2171d7ac2
WPK2006 <william06.a@gmail.com>
2025-11-07 13:59:10 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// IMPORTANT: Ändra 'mastermind' här om ditt repo heter något annat.
 export default defineConfig({
   plugins: [react()],
-  base: '/mastermind/'
+  base: '/mastermind/'    // MÅSTE matcha repo-namnet
 })
