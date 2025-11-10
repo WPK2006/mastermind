fcfe2186d749bd621aafe240ebaad016fce51eae
WPK2006 <william06.a@gmail.com>
2025-11-10 12:50:27 +0100

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Viktigt: base ska matcha repo-namnet på GitHub Pages
-export default defineConfig({
-  plugins: [react()],
-  base: '/mastermind/'
-})
